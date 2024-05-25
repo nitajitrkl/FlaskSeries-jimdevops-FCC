95d02e3c448bc25abb62f03b3c7b4b935261a6b7
Ajit Behera <nitajitrkl@gmail.com>
2024-05-25 15:49:42 -0400

    25th May 2024

--- 02 - Styling and Templates/market.py
+++ 02 - Styling and Templates/market.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
+
 @app.route('/')
+def index():
+    return 'Hello World!!!'
+
+
 @app.route('/home')
 def home_page():
     return render_template('home.html')
